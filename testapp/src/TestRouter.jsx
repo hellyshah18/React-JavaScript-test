d00d5a6d2a74bac31246b217d819b2b74ac657d9
Helly Shah <hellyshah018@gmail.com>
2024-04-14 10:56:28 +0530

    hi

--- testapp/src/TestRouter.jsx
+++ testapp/src/TestRouter.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link} from "react-router-dom";
 import { FakeStoreAPIProducts } from './FakeStoreAPIProducts';
 import { TestForm } from './Form';
 
+
+
 export const TestRouter = () => {
   return (
+    <>
+    <Link  to="/account">Navigate</Link>
     <Routes>
           <Route path="products" element={<FakeStoreAPIProducts />} />
           <Route path="contact" element={<TestForm />} />
-      </Routes>
+      </Routes></>
   )
 }
